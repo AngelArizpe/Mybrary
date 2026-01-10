6ef6105719dc28e0fdc6e1e6362d338a4c520464
AngelArizpe <angeleduardo.arizpe@gmail.com>
2026-01-09 22:53:35 -0600

    File upload database

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,14 @@
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+)
+
+FilePond.setOptions({
+    // Dimensions already defined on the books/index.ejs
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+
+FilePond.parse(document.body)
