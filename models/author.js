390499953dab0e02db09da0b3a235c304ee3175c
AngelArizpe <angeleduardo.arizpe@gmail.com>
2026-01-10 01:53:45 -0600

    Finished author routes

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
@@ -6,5 +7,17 @@ const authorSchema = new mongoose.Schema({
         required: true
     }
 })
+
+authorSchema.pre(
+    'deleteOne',
+    { document: true, query: false },
+    async function () {
+        const books = await Book.countDocuments({ author: this._id })
+
+        if (books > 0) {
+            throw new Error('This author has books still')
+        }
+    }
+)
 
 module.exports = mongoose.model('Author', authorSchema)
